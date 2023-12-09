86585bcbb0025af40103160203f466e54efb3fe5
Alexandr Lisovoy <sashaliss18@gmail.com>
2023-12-09 17:59:47 +0500

    devices block add

--- src/js/index.js
+++ src/js/index.js
@@ -54,6 +54,35 @@ window.addEventListener('DOMContentLoaded', () => {
                 delay: 1500
             }
         }
+    );
+
+    resizeSwiper(
+        '(max-width: 580px)',
+        '.slider-devices', // отдельный класс для этого свайпера
+        {
+            direction: 'horizontal',
+            loop: false,
+            spaceBetween: 16,
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1.3
+                },
+                360: {
+                    slidesPerView: 1.6
+                },
+                470: {
+                    slidesPerView: 2
+                }
+            },
+            autoplay: {
+                delay: 1500
+            }
+        }
     )
 })
 
@@ -70,12 +99,25 @@ readBtn.addEventListener('click', function () {
 })
 
 // brands block show btn
-const showBtn = document.querySelector('.btn-show')
-const listBrands = document.querySelector('.brands__list')
+const brandsContainer = document.querySelector('.brands__container')
+const brandsList = brandsContainer.querySelector('.brands__list')
+const brandsShowBtn = brandsContainer.querySelector('.btn-show')
 
-showBtn.addEventListener('click', function () {
+brandsShowBtn.addEventListener('click', function () {
     this.classList.toggle('btn--more--active')
-    listBrands.classList.toggle('brands__list--visible')
+    brandsList.classList.toggle('brands__list--visible')
+    this.textContent =
+        this.textContent === 'Показать все' ? 'Скрыть' : 'Показать все'
+})
+
+// devices block show btn
+const devicesContainer = document.querySelector('.devices__container');
+const devicesList = devicesContainer.querySelector('.devices__list');
+const devicesShowBtn = devicesContainer.querySelector('.btn-show');
+
+devicesShowBtn.addEventListener('click', function () {
+    this.classList.toggle('btn--more--active')
+    devicesList.classList.toggle('devices__list--visible')
     this.textContent =
         this.textContent === 'Показать все' ? 'Скрыть' : 'Показать все'
 })
